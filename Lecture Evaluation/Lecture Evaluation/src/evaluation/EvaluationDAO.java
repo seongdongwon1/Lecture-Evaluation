0e2b658a819ffbfd5fa686daded4cc62c64225ca
sD <49185772+seongdongwon1@users.noreply.github.com>
2021-03-19 01:32:22 +0900

    20210319
    1. 강의 평가 등록하기 기능 구현.
     - 강의평가 등록하기 DB 와 java 연동
     - select tag textarea tag 등에 선택하고 입력한 값 DB에 잘 저장 되는지 테스트 진행.
    ※ 변수명 오류 확인 제대로 할 것. 변수명이 일치하지 않으면, 값을 제대로 가져오지 못해서 입력하지 않은 값이라는 오류가 나옴.

---
+++ Lecture Evaluation/Lecture Evaluation/src/evaluation/EvaluationDAO.java
@@ -0,0 +1,80 @@
+package evaluation;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import util.DatabaseUtil;
+
+public class EvaluationDAO {
+	public int write(EvaluationDTO evaluationDTO)
+	{
+		String SQL = "INSERT INTO EVALUATION VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0);";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try
+		{
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, evaluationDTO.getUserID());
+			pstmt.setString(2, evaluationDTO.getLectureName());
+			pstmt.setString(3, evaluationDTO.getProfessorName());
+			pstmt.setInt(4, evaluationDTO.getLectureYear());
+			pstmt.setString(5, evaluationDTO.getSemesterDivide());
+			pstmt.setString(6, evaluationDTO.getLectureDivide());
+			pstmt.setString(7, evaluationDTO.getEvaluationTitle());
+			pstmt.setString(8, evaluationDTO.getEvaluationContent());
+			pstmt.setString(9, evaluationDTO.getTotalScore());
+			pstmt.setString(10, evaluationDTO.getCreditScore());
+			pstmt.setString(11, evaluationDTO.getComfortableScore());
+			pstmt.setString(12, evaluationDTO.getLectureScore());
+			//executeQuery : 데이터베이스에서 조회할때 사용함
+			return pstmt.executeUpdate();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		//마지막으로 리셋시키는 것.
+		finally
+		{
+			try
+			{
+				if(conn != null)
+				{
+					conn.close();
+				}
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+			
+			try
+			{
+				if(pstmt != null)
+				{
+					pstmt.close();
+				}
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+			
+			try
+			{
+				if(rs != null)
+				{
+					rs.close();
+				}
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		return -2; //데이터베이스 오류
+	}
+}
